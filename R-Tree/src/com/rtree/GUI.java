bf9ba89a03f502191b3501219065ba60c494b6f8
Peter Boyd <peterboyd192@gmail.com>
2014-04-11 03:21:10 -0400

    GUI Updated
    - Algorithm has rectangle list getter and setter
    - GUI input sets rectangle list
    - Algorthm outputs x and y coordinates from each rectangle

---
+++ R-Tree/src/com/rtree/GUI.java
@@ -0,0 +1,66 @@
+package com.rtree;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+
+public class GUI extends PApplet {
+
+	public static ArrayList<Rectangle> rectList;
+	private Algorithm algo;
+	String words = "Click Somewhere!";
+	int width = 640;
+	int height = 360;
+	int count = 0;
+
+	public void setup() {
+		rectList = new ArrayList<Rectangle>();
+
+		algo = new Algorithm(rectList);
+		size(640, 360);
+		background(250, 250, 250, 250);
+		textFont(createFont("Georgia", 36));
+		textSize(36);
+		fill(0, 0, 0, 100);
+		text(words, 160, 150);
+	}
+
+	public void draw() {
+		for (Rectangle rect : rectList) {
+			rect(rect.x, rect.y, 50, 50);
+		}
+	}
+
+	public void mousePressed() {
+		if (count < 1) {
+			count++;
+			background(250, 250, 250, 250);
+		}
+		if (mousePressed) {
+			addRect(mouseX, mouseY, 60, 60);
+			
+//			ellipse(mouseX-10, mouseY-10, 5, 5);
+			fill(0, 0, 15, 100);
+		}
+	}
+	
+	public void addRect(float x, float y, float w, float h) {
+		Rectangle rectangle = new Rectangle();
+		rectangle.x = (int) x;
+		rectangle.y = (int) y;
+		rectangle.width = (int) w;
+		rectangle.height = (int) h;
+		rectList.add(rectangle);
+		
+		algo.setRectList(rectList);
+	}
+	
+	public ArrayList<Rectangle> getRectList() {
+		return rectList;
+	}
+	
+	public void setRectList() {
+		this.rectList = rectList;
+	}
+}
