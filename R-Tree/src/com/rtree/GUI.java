b421be34b335adb14347697fe4fa64e5b4cb910e
Peter Boyd <peterboyd192@gmail.com>
2014-04-13 17:16:54 -0400

    GUI updated
    - Inserts rectangles given 2 points
    - Deletes rectanlge if one point is clicked
    - Search box shows all rectangles within box

--- R-Tree/src/com/rtree/GUI.java
+++ R-Tree/src/com/rtree/GUI.java
@@ -16,31 +16,53 @@ public class GUI extends PApplet {
 	boolean searchClicked = false;
 	boolean deleteClicked = false;
 	boolean enterClicked = false;
+	boolean searchDone = false;
 	Color regColor = new Color(20, 20, 20, 255);
-	Color clickedColor = new Color(250, 250, 255, 255);
+	Color pressedColor = new Color(250, 50, 50, 255);
 	Color insertColor = new Color(1);
 	Color searchColor = new Color(1);
 	Color deleteColor = new Color(1);
-	Color enterColor = new Color(1);
-	int panelX = 570;
+	int panelX = 580;
 	int insertY = 100;
-	int searchY = 180;
-	int deleteY = 260; 
-	int enterY = 340;
+	int searchY = 200;
+	int deleteY = 300; 
 	int buttonWidth = 150;
 	int buttonHeight = 50;
-
+	int contX = 20;
+	int contY = 20; 
+	int contWidth = 500;
+	int contHeight = 450;
+	int pointCount = 0;
+	
+	// Points for rectangle
+	Point p1; 
+	Point p2;
+	
+	//holds existing points (they are held in rectangle objects)
+	private static ArrayList<Point> pointList = new ArrayList<Point> ();
+	
+	Rectangle searchSpace;	//holds current search rectangle
+	int searchRectX;
+	int searchRectY;
+	int searchRectWidth;
+	int searchRectHeight;
+	
+	//keeps track of current mode
+	public static enum Mode {start, insert, delete, search};
+	Mode mode;
+	
 	public void setup() {
 		rectList = new ArrayList<Rectangle>();
-
+		mode = mode.start;
+		
 		// Window
 		size(800, 500);
 		background(250, 250, 250, 250);
-		
 	}
 
 	public void draw() {
 		// Container
+		fill(250, 250, 250, 250);
 		rect(20, 20, 500, 450);
 		stroke(60, 30, 30, 255);
 		strokeWeight(2);
@@ -56,40 +78,159 @@ public class GUI extends PApplet {
 		rect(panelX, searchY, buttonWidth, buttonHeight);
 		fill(deleteColor.getRGB(), 255);
 		rect(panelX, deleteY, buttonWidth, buttonHeight);
-		fill(enterColor.getRGB(), 255);
-		rect(panelX, enterY, buttonWidth, buttonHeight);
 	
 		// Button Text
 		fill(240, 245, 245, 255);
 		text(insert, panelX+45, insertY+30);
 		text(search, panelX+45, searchY+30);
 		text(delete, panelX+45, deleteY+30);
-		text(enter, panelX+48, enterY+30);
-
+		
+		// If not searching, draw all points
+		if (mode != mode.search) {
+			// Rectangle testing
+			for (Point p: pointList) {
+				fill(0, 0, 15, 100);
+				ellipse(p.x, p.y, 5, 5);
+			}
+			noFill();
+			for (Rectangle r: rectList)
+				drawRect(r);
+		}
+		
+		// If searching, draw rectangles within selected region
+		if (mode == mode.search && searchSpace != null) {
+			if (contains(searchSpace.p1.x, searchSpace.p1.y, contX, contY, contWidth, contHeight)) {
+				drawRect(searchSpace);
+				for (Rectangle r: rectList) {
+					if (contains(r.p1.x, r.p1.y, searchRectX, searchRectY, searchRectWidth, searchRectHeight) && 
+							contains(r.p2.x, r.p2.y, searchRectX, searchRectY, searchRectWidth, searchRectHeight)) {
+						noFill();
+						drawRect(r);
+					}
+				}
+			}
+		}
+		
+		// Set button pressed colors
+		setPressedColors();
 	}
-
+		
 	public void mousePressed() {
-		if (insertClicked(mouseX, mouseY)) { 
-			insertColor = clickedColor;
+		if (insertClicked(mouseX, mouseY)) {
+			mode = mode.insert;
 		} 
 		if (searchClicked(mouseX, mouseY)) { 
-			searchColor = clickedColor;
+			mode = mode.search;
 		} 
-		if (deleteClicked(mouseX, mouseY)) { 
-			deleteColor = clickedColor;
+		if (deleteClicked(mouseX, mouseY)) {
+			mode = mode.delete;
 		} 
-		if (enterClicked(mouseX, mouseY)) { 
-			enterColor = clickedColor;
-		} 
-	}
+		
+		// What to do based on mode
+		modeBehavior(); 
+	}	
 	
 	public void mouseReleased() {
 		insertColor = regColor;
 		searchColor = regColor;
 		deleteColor = regColor;
-		enterColor = regColor;
+		
+		if (mode == mode.search) {
+			searchDone = true;
+		}
 	}
-
+	
+	public void mouseDragged() {
+		if (mode == mode.search) {	// Set bounds for search rectangle
+			if (contains(mouseX, mouseY, contX, contY, contWidth, contHeight)) {
+				Point p1 = new Point(searchRectX, searchRectY);
+				Point p2 = new Point(mouseX, mouseY);
+				searchRectWidth = p2.x - p1.x;
+				searchRectHeight = p2.y - p1.y;
+				searchSpace = new Rectangle(p1, p2, 0, null);
+			}
+		}
+	}
+	
+	// Checks mode and applies actions
+	private void modeBehavior() {	
+		switch (mode)	{
+			case insert:
+				if (contains(mouseX, mouseY, contX, contY, contWidth, contHeight))
+					if (!pointExists (mouseX, mouseY))
+						pointCount++;
+						if (pointCount == 1) {
+							p1 = new Point(mouseX, mouseY);
+							pointList.add(p1);
+						} else if (pointCount == 2) {
+							p2 = new Point(mouseX, mouseY);
+							pointList.add(p2);
+							Rectangle r = new Rectangle (p1, p2, -1, null);
+							rectList.add(r);
+							pointCount = 0;
+						}
+				break;
+			case delete:
+				if (pointExists (mouseX, mouseY)) {
+					for (int i = 0; i < pointList.size(); i++) {
+						Point point = pointList.get(i);
+						if (Math.pow(mouseX - pointList.get(i).x,2) + Math.pow(mouseY - pointList.get(i).y,2) <= 25) {
+							for (int j = 0; j < rectList.size(); j++) {
+								Rectangle rect = rectList.get(j);
+								if (pointOfRect(point, rect)) { 
+									System.out.println(rect.p1.x + "  " + rect.p1.y);
+									rectList.remove(j);
+									pointList.remove(p1);
+									pointList.remove(p2);
+								}	
+							}
+						}
+					}
+				}
+				break;
+			case search:
+				searchRectX = 0;		// Reset search rectangle values
+				searchRectY = 0;
+				searchRectWidth = 0;
+				searchRectHeight = 0;
+				if (contains(mouseX, mouseY, contX, contY, contWidth, contHeight)) {
+					searchRectX = mouseX;
+					searchRectY = mouseY;
+				}
+				searchDone = false;
+				break;
+		}
+	}
+	
+	// Adds rectangle to the display
+	public void drawRect(Rectangle r) {
+		noFill();
+		int x = r.p1.x;
+		int y = r.p1.y;
+		int w = r.p2.x - r.p1.x;
+		int h = r.p2.y - r.p1.y;
+		rect(x, y, w, h);
+	}
+	
+	// Determines if a point exists at given coordinates
+	public boolean pointExists(int x, int y) {
+		for (int i = 0; i < pointList.size(); i++) {
+			if (Math.pow(x - pointList.get(i).x,2) + Math.pow(y - pointList.get(i).y,2) <= 25)
+				return true;
+		}
+		return false;
+	}
+	
+	// Set color of pressed buttons
+	public void setPressedColors() {
+		if (mode == mode.insert)
+			insertColor = pressedColor;
+		if (mode == mode.delete)
+			deleteColor = pressedColor;
+		if (mode == mode.search)
+			searchColor = pressedColor;
+	}
+	
 	public ArrayList<Rectangle> getRectList() {
 		return rectList;
 	}
@@ -98,35 +239,48 @@ public class GUI extends PApplet {
 		this.rectList = rectList;
 	}
 	
-	public boolean insertClicked(float mouseX, float mouseY) {
-		if (mouseX > panelX && mouseX < panelX+buttonWidth && mouseY > insertY && mouseY < insertY+buttonHeight) {
+	public boolean insertClicked(int mouseX, int mouseY) {
+		if (contains(mouseX, mouseY, panelX, insertY, buttonWidth, buttonHeight)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public boolean searchClicked(float mouseX, float mouseY) {
-		if (mouseX > panelX && mouseX < panelX+buttonWidth && mouseY > searchY && mouseY < searchY+buttonHeight) {
+	public boolean searchClicked(int mouseX, int mouseY) {
+		if (contains(mouseX, mouseY, panelX, searchY, buttonWidth, buttonHeight)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public boolean deleteClicked(float mouseX, float mouseY) {
-		if (mouseX > panelX && mouseX < panelX+buttonWidth && mouseY > deleteY && mouseY < deleteY+buttonHeight) {
+	public boolean deleteClicked(int mouseX, int mouseY) {
+		if (contains(mouseX, mouseY, panelX, deleteY, buttonWidth, buttonHeight)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public boolean enterClicked(float mouseX, float mouseY) {
-		if (mouseX > panelX && mouseX < panelX+buttonWidth && mouseY > enterY && mouseY < enterY+buttonHeight) {
+	// If first two values are within rectangle bounds of last 4 values
+	public boolean contains(int x, int y, int rectX, int rectY, int rectWidth, int rectHeight) {
+		if (x > rectX && x < rectX+rectWidth && y > rectY && y < rectY+rectHeight) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+	
+	// If point given is part of the rectangle given
+	public boolean pointOfRect(Point point, Rectangle rect) {
+		if (point.x == rect.p1.x && point.y == rect.p1.y || point.x == rect.p2.x && point.y == rect.p2.y) {
+			p1 = rect.p1;
+			p2 = rect.p2;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
